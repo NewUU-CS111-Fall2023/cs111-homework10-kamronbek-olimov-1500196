d08816857686b8111b27e267f73595e314557dc1
kamronbek-olimov-1500196 <127713658+kamronbek-olimov-1500196@users.noreply.github.com>
2024-01-19 19:39:30 +0500

    Update task_1.cpp

--- src/cpp/task_1.cpp
+++ src/cpp/task_1.cpp
@@ -1,9 +1,27 @@
-/*
- * Author:
- * Date:
- * Name:
- */
-
-class Problem1 {
+class problem1 {
 public:
+    ListNode* mergeNodes(ListNode* head) {
+        if(head == NULL) {
+            return NULL;
+        }
+
+        ListNode* current = head->next;
+        ListNode* node = head;
+        int sum = 0;
+
+        while(current) {
+            while(current->val != 0) {
+                sum += current->val;
+                current = current->next;
+            }
+
+            node->next = new ListNode(sum);
+            node = node->next;
+
+            sum = 0;
+            current = current->next;
+        }
+
+        return head->next;
+    }
 };
